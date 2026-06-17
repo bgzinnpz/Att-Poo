efce732ba1849766c4327f84069b2693c1b0e2a9
Bryan Gabriel <bgzinnpz@gmail.com>
2026-06-17 17:02:39 -0300

    Update lab8_.py

--- lab8_.py
+++ lab8_.py
@@ -2,5 +2,18 @@ class Carteira:
     def __init__(self, moeda, saldo):
         self._moeda = moeda
         self._saldo = saldo
-    def conversor_yuan(self, USD, CNY, BRL)
+        
+    def conversor_yuan(self, valor_yuan):
+       if self.moeda ==  "USD"
+            return valor_yuan * 0.14
+     elif self.moeda == "BRL":
+        return valor_yuan * 0.85
+     elif self.moeda == "CNY":
+        return valor_yuan
+     else:
+        print("Erro")
+
+def __add__(self, valor_yuan):
+    valor_convertido = self.converter_yuan(valor_yuan)
+        
   
