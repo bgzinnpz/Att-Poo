7fee6686be97aa6f7c1b1921b60374bb5ad787fa
Bryan Gabriel <bgzinnpz@gmail.com>
2026-06-17 17:16:45 -0300

    Add addition and subtraction methods for currency

--- lab8_.py
+++ lab8_.py
@@ -15,5 +15,13 @@ class Carteira:
 
 def __add__(self, valor_yuan):
     valor_convertido = self.converter_yuan(valor_yuan)
+    return self.saldo + valor_convertido
+
+def __sub__(self, valor_yuan):
+    valor_convertido = self.converter_yuan(valor_yuan)
+    return self.saldo - valor_convertido
         
-  
+  carteira_usd = Carteira("USD", 10.0)
+# 50 CNY = 7.0 USD (usando taxa 1 CNY = 0.14 USD) 
+print('Soma de carteira USD + 50 yuan = ', carteira_usd + 50.0) # 17.0
+
