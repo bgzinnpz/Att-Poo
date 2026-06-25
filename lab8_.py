d0780ffe2442f0b5ca4084840636e0c6c5d59ff1
Bryan Gabriel <bgzinnpz@gmail.com>
2026-06-25 15:13:31 -0300

    Refactor Carteira class for currency operations
    Refactor currency conversion methods and add operator overloading for addition and subtraction.

--- lab8_.py
+++ lab8_.py
@@ -1,27 +1,22 @@
 class Carteira:
     def __init__(self, moeda, saldo):
-        self._moeda = moeda
-        self._saldo = saldo
-        
-    def conversor_yuan(self, valor_yuan):
-       if self.moeda ==  "USD"
-            return valor_yuan * 0.14
-     elif self.moeda == "BRL":
-        return valor_yuan * 0.85
-     elif self.moeda == "CNY":
-        return valor_yuan
-     else:
-        print("Erro")
-
-def __add__(self, valor_yuan):
-    valor_convertido = self.converter_yuan(valor_yuan)
-    return self.saldo + valor_convertido
-
-def __sub__(self, valor_yuan):
-    valor_convertido = self.converter_yuan(valor_yuan)
-    return self.saldo - valor_convertido
-        
-  carteira_usd = Carteira("USD", 10.0)
-# 50 CNY = 7.0 USD (usando taxa 1 CNY = 0.14 USD) 
-print('Soma de carteira USD + 50 yuan = ', carteira_usd + 50.0) # 17.0
-
+        self.moedas = moeda
+        self.saldo = saldo
+    def __add__ (self, other):
+        if isinstance(other, CNY):
+            return saldo (f"o saldo agora é " self.saldo + self.other)
+        elif isinstance(other, USD):
+            saldo = other * 0.14
+            return saldo (f"o saldo agora é " self.saldo + self.other)
+        elif isinstance(other, BRL):
+            saldo = other * 0.85
+            return saldo (f"o saldo agora é " self.saldo + self.other)
+     def __sub__ (self, other):
+        if isinstance(other, CNY):
+            return saldo (f"o saldo agora é " self.saldo - self.other)
+        elif isinstance(other, USD):
+            saldo = other * 0.14
+            return saldo (f"o saldo agora é " self.saldo - self.other)
+        elif isinstance(other, BRL):
+            saldo = other * 0.85
+            return saldo (f"o saldo agora é " self.saldo - self.other)
